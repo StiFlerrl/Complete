f2613f66c429fcd17a855ffd5b7aa4f10df3bc0b
StiFlerrl <stif872@gmail.com>
2025-07-03 22:26:12 +0300

    Update Data_Check_alerts.js

--- Data_Check_alerts.js
+++ Data_Check_alerts.js
@@ -5,6 +5,7 @@
 // @description  Info check + alerts with field comparison and plan detection
 // @match        https://emdspc.emsow.com/*
 // @grant        none
+// @homepageURL  https://github.com/StiFlerrl/Complete
 // @updateURL    https://raw.githubusercontent.com/StiFlerrl/Complete/main/Data_Check_alerts.js
 // @downloadURL  https://raw.githubusercontent.com/StiFlerrl/Complete/main/Data_Check_alerts.js
 
